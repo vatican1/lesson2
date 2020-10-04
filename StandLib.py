34f39f587fde2f34ffc12413f7ae9a873ca21aa3
Viktor <vatican2001@gmail.com>
2020-10-04 21:11:07 +0300

    сделал счасть второй части

--- StandLib.py
+++ StandLib.py
@@ -11,12 +11,18 @@ def show_sizeof(x, level=0):
                 for xx in x:
                     show_sizeof(xx, level + 1)
 
-
+d_letter= {}
 d = {}
 file = open('text.txt', 'r')
 for line in file:
     line = line.rstrip()
     for words in line.split(' '):
+        for letters in words:
+            if d_letter.get(letters) != None:
+                d_letter[letters] += 1
+            else:
+                d_letter[letters] = 1
+
         if d.get(words) != None:
             d[words] += 1
         else:
@@ -28,4 +34,11 @@ for i in d:
     if d[i]>max:
         max = d[i]
         word = i
-print(max, i)
+print(max, word)
+max = 0
+letter = 'a'
+for i in d_letter:
+    if d_letter[i]>max:
+        max = d_letter[i]
+        letter = i
+print(max, letter)
